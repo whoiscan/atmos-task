a1d0a90da3f229c1b342e8be187be7b0c8e90428
Islamov Jr <jamhurj@gmail.com>
2021-10-25 11:07:19 +0500

    feat: minor changes

--- src/main/java/com/example/atmostask/models/custom/ResponseMessages.java
+++ src/main/java/com/example/atmostask/models/custom/ResponseMessages.java
@@ -1,7 +1,7 @@
 package com.example.atmostask.models.custom;
 
 public interface ResponseMessages {
-    String USER_NAME_REQ = "Логин не может пустым!";
+    String USER_NAME_REQ = "Логин не может быть пустым!";
     String EMAIL_REQ = "Адрес электронной почты не может быть пустым!";
     String NOT_VALID_EMAIL_REQ = "Введите действующий адрес электронной почты!";
     String NOT_VALID_PASSWORD_REQ = "Пароль должен содержать от 5 до 16 символов!";
